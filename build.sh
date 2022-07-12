be4f55ac21bb7fa47b14674a9d54f7f8a256a829
Y. Z. Chen <754097987@qq.com>
2022-07-13 02:42:37 +0800

    [Update] framework.

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 [[ -n $BUILD_COUNT_SERVER ]] && curl -so build.txt "$BUILD_COUNT_SERVER"/CST-Live-Guide4 || echo -n "LOCAL" > build.txt
+
 rm -rf build
+chapters=$(find * -maxdepth 1 -type d)
 mkdir build
+for chapter in $chapters; do
+  mkdir build/"$chapter"
+done
 xelatex -output-directory=build -shell-escape CST-Live-Guide.tex
 xelatex -output-directory=build -shell-escape CST-Live-Guide.tex
 cp build/CST-Live-Guide.pdf CST-Live-Guide.pdf
