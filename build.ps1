c8dded83299f057077d372da1b303c1be68ef55d
Y. Z. Chen <754097987@qq.com>
2022-07-17 13:33:10 +0800

    [Update] now it is possible to add references.

--- build.ps1
+++ build.ps1
@@ -12,6 +12,8 @@ New-Item -ItemType Directory build
 Get-ChildItem -Directory -Exclude .idea,build | ForEach-Object {
     New-Item -ItemType Directory build/$($_.Name)
 }
-xelatex -output-directory=build -shell-escape CST-Live-Guide.tex
-xelatex -output-directory=build -shell-escape CST-Live-Guide.tex
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
+biber --output-directory=build CST-Live-Guide
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
 Copy-Item -Path build/CST-Live-Guide.pdf -Destination CST-Live-Guide.pdf
--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ mkdir build
 for chapter in $chapters; do
   mkdir build/"$chapter"
 done
-xelatex -output-directory=build -shell-escape CST-Live-Guide.tex
-xelatex -output-directory=build -shell-escape CST-Live-Guide.tex
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
+biber --output-directory=build CST-Live-Guide
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
 cp build/CST-Live-Guide.pdf CST-Live-Guide.pdf
