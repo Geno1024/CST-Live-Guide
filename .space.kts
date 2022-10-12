2801f09762ecd9cf35fe4baa16bb577d2a27ca1a
Y. Z. Chen <754097987@qq.com>
2022-10-13 01:43:58 +0800

    [Pipeline] can I do this?

--- .space.kts
+++ .space.kts
@@ -1,11 +1,5 @@
 job("Build") {
-    host {
-        shellScript("Install TeXLive and Fonts") {
-            content = """
-                apt update
-                apt install texlive-xetex texlive-lang-chinese texlive-bibtex-extra biber texlive-science
-            """
-        }
+    container("texlive/texlive") {
         shellScript("Build") {
             location = "./build.sh"
         }
