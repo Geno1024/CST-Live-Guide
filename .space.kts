3e4c9641793e0a2cc17f84fcfb425bf1c9778953
Y. Z. Chen <754097987@qq.com>
2022-10-13 01:31:14 +0800

    [Pipeline] does installation requires sudo?

--- .space.kts
+++ .space.kts
@@ -1,6 +1,12 @@
-job("Main") {
-    host("build") {
-        shellScript {
+job("Build") {
+    host {
+        shellScript("Install TeXLive and Fonts") {
+            content = """
+                apt update
+                apt install texlive-xetex texlive-lang-chinese texlive-bibtex-extra biber texlive-science
+            """
+        }
+        shellScript("Build") {
             location = "./build.sh"
         }
     }
