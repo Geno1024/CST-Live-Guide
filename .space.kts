95747fe7fd921b7e3d71abe211cfb48d4ad5c67e
Y. Z. Chen <754097987@qq.com>
2022-10-13 02:04:21 +0800

    [Pipeline] build count.

--- .space.kts
+++ .space.kts
@@ -1,5 +1,6 @@
 job("Build") {
     container("texlive/texlive") {
+        env["BUILD_COUNT_SERVER"] = Secrets("build_count_server")
         shellScript("Build") {
             location = "./build.sh"
         }
