c26c809d20167a7743aa6e65ccba167c0c759483
Y. Z. Chen <754097987@qq.com>
2022-10-13 08:04:06 +0800

    [Pipeline] not bash on docker:texlive/texlive?

--- .space.kts
+++ .space.kts
@@ -2,6 +2,7 @@ job("Build") {
     container("texlive/texlive") {
         env["BUILD_COUNT_SERVER"] = Secrets("build_count_server")
         shellScript("Build") {
+            interpreter = "/bin/bash"
             location = "./build.sh"
         }
     }
