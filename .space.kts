25863b3f61a3838180c053cebea9a6b9b1388654
Y. Z. Chen <754097987@qq.com>
2022-10-14 02:06:02 +0800

    [Pipeline] upload to Space file repository.

--- .space.kts
+++ .space.kts
@@ -1,9 +1,18 @@
 job("Build") {
     container("texlive/texlive") {
         env["BUILD_COUNT_SERVER"] = Secrets("build_count_server")
+        env["FILE_REPO_ADDRESS"] = Secrets("file_repo_address")
         shellScript("Build") {
             interpreter = "/bin/bash"
             location = "./build.sh"
+        }
+        shellScript("Publish") {
+            content = """
+                SOURCE_PATH=CST-Live-Guide.pdf
+                TARGET_PATH=CST-Live-Guide.pdf
+                REPO_URL=${'$'}FILE_REPO_ADDRESS
+                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
+            """.trimIndent()
         }
     }
 }
