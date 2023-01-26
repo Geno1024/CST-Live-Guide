27f951d3c83f8b4b8016122cb527d6f24a1c7c6a
Y. Z. Chen <754097987@qq.com>
2023-01-26 13:17:12 +0800

    [Project] add a Windows 7 compatible Powershell build script.

---
+++ build.windows7.ps1
@@ -0,0 +1,16 @@
+if ($env:BUILD_COUNT_SERVER -eq $nul -or $env:BUILD_COUNT_SERVER.trim() -eq "")
+{
+    Write-Output "LOCAL" | Out-File -FilePath build.txt
+}
+else
+{
+    (New-Object System.Net.WebClient).DownloadFile("$env:BUILD_COUNT_SERVER/CST-Live-Guide4", "build.txt")
+}
+(detex ./CST-Live-Guide | Measure-Object -Character).Characters > wordcount.txt
+Remove-Item -Recurse build
+New-Item -ItemType Directory build
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
+biber --output-directory=build CST-Live-Guide
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
+xelatex -output-directory=build -shell-escape -synctex=1 CST-Live-Guide.tex
+Copy-Item -Path build/CST-Live-Guide.pdf -Destination CST-Live-Guide.pdf
